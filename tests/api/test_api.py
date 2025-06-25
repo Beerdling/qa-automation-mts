47f9b08bc7732804b3968734c802d049f30a9b84
Alexey <zephyr88pro@gmail.com>
2025-06-25 13:27:24 +0300

    Update test_api.py
    Исправлена синтаксическая ошибка

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,9 +1,13 @@
 import requests
 import requests_mock
 
-def test_api_is_working():
+def test_api():
     with requests_mock.Mocker() as m:
-        m.get("https://jsonplaceholder.typicode.com/posts/1, text='{"status": "ok"}', status_code=200)
+        m.get(
+            "https://jsonplaceholder.typicode.com/posts/1",
+            json={"status": "ok"},  # Используем json вместо text
+            status_code=200
+        )
         response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
         assert response.status_code == 200
-test_api_is_working()
+        assert response.json()["status"] == "ok"
